410effec1d23c10364684004869c3f8d8740dba3
tokarro666 <tokarro666@gmail.com>
2022-07-23 15:02:54 +0200

    pętla z while i dowhile

---
+++ src/WhileLoop.java
@@ -0,0 +1,13 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        int licznik = 0;
+ //       while(licznik < 10){
+ //           licznik++;
+ //           System.out.println(licznik);
+ //     }
+    do{
+        licznik++;
+        System.out.println(licznik);
+    } while (licznik < 10);
+    }
+}
