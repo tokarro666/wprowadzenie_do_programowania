7825de0acacf71ec4fb00a43c06eec0364746bf9
tokarro666 <tokarro666@gmail.com>
2022-07-23 12:52:39 +0200

    stworzyłem testowanie inkrementacji

---
+++ src/ForLoop.java
@@ -0,0 +1,13 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        int x = 3;
+        int y;
+        System.out.println(x++);
+        System.out.println(++x);
+        y = x++;
+        System.out.println(y);
+        y = ++x;
+        System.out.println(y);
+        System.out.println(++y);
+    }
+}
--- src/TextFromUser.java
+++ src/TextFromUser.java
@@ -1,8 +1,14 @@
 import java.util.Scanner;
 public class TextFromUser {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
-        String  userInput = scanner.nextLine();
-        System.out.println("Witaj Podróżniku "+userInput);
+        System.out.println("Podaj imie");
+        String  imie = scanner.nextLine();
+        System.out.println("Podaj nazwisko");
+        String nazwisko = scanner.nextLine();
+        System.out.println("Podaj wiek");
+        int wiek = scanner.nextInt();
+        System.out.println("Witaj " +imie +" " +nazwisko +" . Masz " +wiek +" lat.");
     }
 }
