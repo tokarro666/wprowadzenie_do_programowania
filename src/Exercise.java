1fe213e00c15bab1553aa8b3c9a94335834b56af
tokarro666 <tokarro666@gmail.com>
2022-07-23 14:15:36 +0200

    pętla z blokadą wiekową i informacją o wygranej

--- src/Exercise.java
+++ src/Exercise.java
@@ -6,6 +6,7 @@ public class Exercise {
          * 1. chcę aby w programie było o imię i nazwisko oraz wiek
          *  2. chcę aby była pętla sprawdzająca czy osoba jest pełnoletnia, jeżeli nie to napis wypad, jeżeli tak, to zaproszenie
          */
+        String[] wygrani = new String[]{"Ania", "Zosia", "Magda", "Piotrek", "Adam"};
         Scanner scanner = new Scanner(System.in);
         System.out.println("Podaj Imię");
         String imie = scanner.next();
@@ -15,6 +16,12 @@ public class Exercise {
         int wiek = scanner.nextInt();
         if (wiek >= 18) {
             System.out.println("Zapraszam do klubu");
+            for (String wygrany: wygrani){
+                if (imie.equals(wygrany)){
+                    System.out.println("Dostajesz darmowego szota ;) ");
+                }
+
+            }
         }
         if (wiek < 18) {
             System.out.println("Wypad z klubu kaszojadzie");
